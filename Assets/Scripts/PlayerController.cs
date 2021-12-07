b81cf5e5417979d47ff59759204df5bccb10151b
Saulo Caliman <saulocalimangomes@gmail.com>
2021-12-07 12:45:34 -0300

    players

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 using Mirror;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
 
+    public Cinemachine.CinemachineVirtualCamera cvCam;
+    public Camera mainCamera;
+
     public Image playerLifeImg;
+
+    public Canvas myCanvas;
+
+    [SyncVar]
+    public string playerId;
 
     bool knockback = false;
     float knockbackTime;
@@ -19,11 +27,13 @@ public class PlayerController : MonoBehaviour
 
     public float atkDelay;
     
+    [SyncVar]
     public float countAtkDelay;
 
     Rigidbody2D rb2D;
 
     Animator animator;
+
 
     float lastX, lastY;
     float hAxis, vAxis;
@@ -33,70 +43,80 @@ public class PlayerController : MonoBehaviour
     {
         rb2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        if (isLocalPlayer) {
+            playerId = "Player" + NetworkClient.localPlayer.netId;
+            gameObject.name = playerId;
+            cvCam.gameObject.SetActive(true);
+            mainCamera.gameObject.SetActive(true);
+            myCanvas.gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        hAxis = Input.GetAxis("Horizontal");
-        vAxis = Input.GetAxis("Vertical");
+        if (isLocalPlayer) { 
+            hAxis = Input.GetAxis("Horizontal");
+            vAxis = Input.GetAxis("Vertical");
 
-        if (Mathf.Abs(hAxis) >= 0.000000001) {
-            lastX = hAxis;
-            lastY = 0;
-        }
-        if (Mathf.Abs(vAxis) >= 0.000000001)
-        {
-            lastY = vAxis;
-            lastX = 0;
-        }
-
-        if (countAtkDelay > atkDelay)
-        {
-            if (Input.GetButtonDown("Fire1"))
+            if (Mathf.Abs(hAxis) >= 0.000000001) {
+                lastX = hAxis;
+                lastY = 0;
+            }
+            if (Mathf.Abs(vAxis) >= 0.000000001)
             {
-                countAtkDelay = 0;
-                if (lastX > 0)
+                lastY = vAxis;
+                lastX = 0;
+            }
+        
+            if (countAtkDelay > atkDelay)
+            {
+                if (Input.GetButtonDown("Fire1"))
                 {
-                    PlayAnimation("atk-right");
+                    countAtkDelay = 0;
+                    if (lastX > 0)
+                    {
+                        PlayAnimation("atk-right");
+                    }
+                    else if (lastX < 0)
+                    {
+                        PlayAnimation("atk-left");
+                    }
+                    else if (lastY > 0)
+                    {
+                        PlayAnimation("atk-up");
+                    }
+                    else if (lastY < 0)
+                    {
+                        PlayAnimation("atk-down");
+                    }
                 }
-                else if (lastX < 0)
-                {
-                    PlayAnimation("atk-left");
-                }
-                else if (lastY > 0)
-                {
-                    PlayAnimation("atk-up");
-                }
-                else if (lastY < 0)
-                {
-                    PlayAnimation("atk-down");
+            }
+            else {
+                countAtkDelay += Time.fixedDeltaTime;
+            }
+
+            if (knockback == true) {
+                knockbackTime += Time.fixedDeltaTime;
+                if (knockbackTime > 1) {
+                    knockback = false;
+                    knockbackTime = 0;
                 }
             }
         }
-        else {
-            countAtkDelay += Time.fixedDeltaTime;
-        }
-
-        if (knockback == true) {
-            knockbackTime += Time.fixedDeltaTime;
-            if (knockbackTime > 1) {
-                knockback = false;
-                knockbackTime = 0;
-            }
-        }
-
     }
 
     private void FixedUpdate()
     {
-        Vector2 newPos = new Vector2(hAxis, vAxis).normalized * speed * Time.fixedDeltaTime;
-        if(knockback == false) { 
-            rb2D.velocity = newPos;
+        if (isLocalPlayer) { 
+            Vector2 newPos = new Vector2(hAxis, vAxis).normalized * speed * Time.fixedDeltaTime;
+            if(knockback == false) { 
+                rb2D.velocity = newPos;
         
-            animator.SetFloat("Y", lastY);
+                animator.SetFloat("Y", lastY);
 
-            animator.SetFloat("X", lastX);
+                animator.SetFloat("X", lastX);
+            }
         }
     }
 
@@ -116,19 +136,38 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.tag == "AtkPlayer")
         {
-            health--;
-            playerLifeImg.fillAmount = health / maxHealth;
-            Knockback(collision.gameObject.transform.position);
-            if (health < 1)
-            {
-                PlayAnimation("Death");
-            }
+            //Knockback(collision.gameObject.transform.position);
+            CMDDamage(gameObject);
+        }
+    }
+
+    [Command]
+    void CMDDamage(GameObject target) {
+        NetworkIdentity identidadeOponente = target.GetComponent<NetworkIdentity>();
+        RPCDamage(identidadeOponente.connectionToClient);
+    }
+
+    [TargetRpc]
+    public void RPCDamage(NetworkConnection target) {
+        if (!isServer) return;
+        PlayerController playerTarget = target.identity.GetComponent<PlayerController>();
+        playerTarget.TakeDamage(1);
+    }
+
+    public void TakeDamage(int amount) {
+        health -= amount;
+        playerLifeImg.fillAmount = health / maxHealth;
+        if (health < 1)
+        {
+            PlayAnimation("Death");
         }
     }
 
     public void DestroyGameObject() {
         gameObject.SetActive(false);
     }
+
+
 
     void Knockback(Vector3 pos) {
         knockback = true;
